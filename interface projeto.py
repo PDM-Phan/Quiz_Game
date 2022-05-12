86879347610e6884cd078c427f2565d33c060796
mpssantana <104942232+mpssantana@users.noreply.github.com>
2022-05-12 19:44:07 -0300

    Add files via upload

--- interface projeto.py
+++ interface projeto.py
@@ -1,6 +1,7 @@
 from ast import Or
 from locale import windows_locale
 from PySimpleGUI import PySimpleGUI as sg
+
 #TELA DE INTRODUÇÃO
 
 def JanelaIntrod():
@@ -12,7 +13,7 @@ def JanelaIntrod():
         [sg.Text()],
         [sg.Button('Logar como Administrador'), sg.Button('Logar como Usuário')]
     ]
-    return sg.Window('QUIZ SERES MITOLÓGICOS', Layout = layoutintrod, finalize=True)
+    return sg.Window('QUIZ SERES MITOLÓGICOS', layout= layoutintrod, finalize=True)
 
 def JanelaUsuario():
     sg.theme('DarkGreen7')
@@ -21,10 +22,11 @@ def JanelaUsuario():
         [sg.Text()],
         [sg.Text('Login: '), sg.Input(key = 'loginusuar')],
         [sg.Text('Senha'), sg.Input(key = 'senhausuar', password_char= '*')],
+        [sg.Button('Entrar')],
         [sg.Text()],
         [sg.Button('Cadastrar novo usuário')]
         ]
-    return sg.Window('USUÁRIO(A)', Layout = layoutusuar, finalize=True)
+    return sg.Window('USUÁRIO(A)', layout = layoutusuar, finalize=True)
 
 def JanelaAdm():
     sg.theme('DarkGreen7')
@@ -33,10 +35,11 @@ def JanelaAdm():
         [sg.Text()],
         [sg.Text('Login: '), sg.Input(key = 'loginadm')],
         [sg.Text('Senha'), sg.Input(key = 'senhaadm', password_char= '*')],
+        [sg.Button('Entrar')],
         [sg.Text()],
         [sg.Button('Cadastrar novo usuário')]
         ]
-    return sg.Window('ADMINISTRADOR(A)', Layout = layoutadm, finalize=True)
+    return sg.Window('ADMINISTRADOR(A)', layout = layoutadm, finalize=True)
 
 def JanelaCad():
     sg.theme('DarkGreen7')
@@ -47,22 +50,24 @@ def JanelaCad():
         [sg.Text('Crie um login: '), sg.Input(key = 'novologin')],
         [sg.Text('Crie uma senha:'), sg.Input(key = 'novasenha', password_char= '*')],
         [sg.Text()],
-        [sg.Checkbox('Cadastrar como usuário', key = 'usuario'), sg.Checkbox('Cadastrar como administrador', key = 'adm'),]
+        [sg.Checkbox('Cadastrar como usuário', key = 'usuario'), sg.Checkbox('Cadastrar como administrador', key = 'adm')],
         [sg.Button('Cadastrar')]
         ]
-    return sg.Window('CADASTRO', Layout = layoutcad, finalize=True)
+    return sg.Window('CADASTRO', layout = layoutcad, finalize=True)
 
-def JanelaPerguntas():
-    sg.theme('DarkGreen7')
+def JanelaPerg():
+    sg.theme('DarkGreen5')
     layoutperg = [
+        [sg.Text('Score = #X')],
         [sg.Text('Pergunta: ', '#pergunta#')],
-        [sg.Text('Alternativas: ' + '\nA: #X\nB: #Z\nC: #S\nD: #M\nE: #D')],
+        [sg.Text('Alternativas:')],
+        [sg.Text('\n')],
         [sg.Text('Qual a alternativa correta?')],
-        [sg.Button('A'),sg.Button('B'),sg.Button('C'),sg.Button('D'),sg.Button('E'),],
-        [sg.Text('Score' + ' #pontuação#')],
+        [sg.Button('A'),sg.Button('B'),sg.Button('C'),sg.Button('D'),sg.Button('E')],
         [sg.Button('Próxima pergunta')]
     ]
-    return sg.Window('PERGUNTAS', Layout = layoutperg, finalize=True)
+    return sg.Window('PERGUNTAS', layout=layoutperg, finalize=True)
+
 
 #JANELAS
 
@@ -75,12 +80,36 @@ while True:
     #quando a janela for fechada
     if window == janela1 and evento == sg.WIN_CLOSED:
         break
+    if window == janela2 and evento == sg.WIN_CLOSED:
+        break
+    if window == janela3 and evento == sg.WIN_CLOSED:
+        break
+    if window == janela4 and evento == sg.WIN_CLOSED:
+        break
+    if window == janela5 and evento == sg.WIN_CLOSED:
+        break
 
     #proxima janela
-    if window == janela1 and evento == 'Logar como Administrador':
-        janela2 = JanelaAdm()
-        janela1.hide
-
     if window == janela1 and evento == 'Logar como Usuário':
-        janela3 = JanelaUsuario()
-        janela1.hide
+        janela2 = JanelaUsuario()
+        janela1.hide()
+
+    if window == janela1 and evento == 'Logar como Administrador':
+        janela3 = JanelaAdm()
+        janela1.hide()
+
+    if window == janela2 and evento == 'Cadastrar novo usuário':
+        janela4 = JanelaCad()
+        janela2.hide()
+
+    if window == janela3 and evento == 'Cadastrar novo usuário':
+        janela4 = JanelaCad()
+        janela3.hide()
+
+    if window == janela2 and evento == 'Entrar':
+        janela5 = JanelaPerg()
+        janela2.hide()
+
+    if window == janela3 and evento == 'Entrar':
+        janela5 = JanelaPerg()
+        janela3.hide()
